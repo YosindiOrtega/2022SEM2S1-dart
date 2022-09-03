812ceb5b0a3ea8c1794e659ca67070c4fb664bc3
YosindiOrtega <68757951+YosindiOrtega@users.noreply.github.com>
2022-09-03 16:58:18 -0500

    Update excepciones.dart

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,1 +1,24 @@
-void main() {}
+void main() {
+  try{
+    String elNombre= obtenerNombre("Sindi");
+    print(elNombre);
+  } on FormatException catch(e){
+    print("Se presentó una excepción FormatException =$e");
+  } on Exception catch(e){
+    print("Se presentó una excepción Exception =$e");
+  } catch(e){
+    print("Se presentó una excepción general $e ");
+  } finally{
+    print("Este es el finally");
+  }
+}
+
+  String obtenerNombre(String nombre){
+    if(nombre =='german'){
+      throw FormatException("Este nombre no está permitido");
+    }
+    if(nombre =='Miguel'){
+      throw FormatException("Este nombre no está permitido");
+    }
+    return 'Mi nombre es $nombre';
+  }
