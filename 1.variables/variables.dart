48ec8b893c1bd5efa7bf8c35662cef4e80d9346c
YosindiOrtega <68757951+YosindiOrtega@users.noreply.github.com>
2022-09-03 16:54:19 -0500

    Update variables.dart

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  
+  var nombre ="Sindi1"; //Variable generica , toma el tipo según lo que se le guarde
+  String nombreDos = "Sindi2"; 
+
+  final String nombreTres = "Sindi3";
+  //da error
+  //nombreTres = "Si";
+  print(nombre);
+  print(nombreDos);
+  print(nombreTres); 
+}
