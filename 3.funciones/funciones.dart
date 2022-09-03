293772f66d164e74367cd91e020072e9f87760d1
YosindiOrtega <68757951+YosindiOrtega@users.noreply.github.com>
2022-09-03 16:56:47 -0500

    Update funciones.dart

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,19 @@
-void main() {}
+void main() {
+  print(obtenerNombre("sindi"));
+  print(obtenerNombreApellido("sindi","Ortega"));
+  print(obtenerNombreApellidoNameParameter(apellido: "Ortega"));
+}
+String obtenerNombre(String valor){
+  return 'Mi nombre es $valor';
+  }
+String obtenerNombreApellido(String nombre, String apellido){
+  return 'Mi nombre es $nombre $apellido';
+  }
+String obtenerNombreApellidoNameParameter(
+  {String? nombre, required String apellido, int edad=0}){
+  return 'Mi nombre es $nombre $apellido $edad';
+  }
+  //----------function anonymus
+  var myFuncionAnonima =(String valor){
+    return "el valor es $valor";
+  };
